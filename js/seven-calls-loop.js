40f9f1f6dddd7f42ab7a10e443c0e14066b710b3
Andrés González <89161075+Shevotool@users.noreply.github.com>
2022-09-22 23:18:52 -0600

    Commit inicial

---
+++ js/seven-calls-loop.js
@@ -0,0 +1,16 @@
+/* ***** Exercise 1 ***** */
+
+/*  Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+for (let i = "#"; i.length < 8; i += "#") {
+  /*  console.log(i);*/
+}
